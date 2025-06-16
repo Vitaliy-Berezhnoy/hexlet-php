6687961bc396f8a1301d3ed85ca63f83314cc5d2
Виталий <ipbvv@mail.ru>
2025-06-16 21:41:49 +0300

    add Runner.php

---
+++ src/Runner.php
@@ -0,0 +1,11 @@
+namespace hexlet\php\Runner;
+
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', 'ivan'])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
